25cce4db15c1fc382940a780c03e3c353073b091
hoangnguyen <nghoangn@sheridancollege.ca>
2024-11-25 22:24:56 -0600

    Fourth Comment Adding Name of group member

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -8,8 +8,8 @@ package ca.sheridancollege.project;
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
  *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
+ * @author Hoang Ngoc Tram Nguyen
+ * @author Jacob Cruz
  */
 public abstract class Player {
 
