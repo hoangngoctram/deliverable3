6958d15a73d5b0bf042629ba2304368604a423b3
hoangnguyen <nghoangn@sheridancollege.ca>
2024-11-25 22:32:51 -0600

    Sixth Comment: Create a new class call UnoCard is a subclass of Card

---
+++ src/ca/sheridancollege/project/UnoCard.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author Hoang Nguyen
+ * @author Jacob Cruz
+ */
+public class UnoCard extends Card {
+  private final String color;
+    private final String value;
+
+    public UnoCard(String color, String value) {
+        this.color = color;
+        this.value = value;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return color + " " + value;
+    }
+}
