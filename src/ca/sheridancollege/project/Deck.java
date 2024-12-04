529e99e81796a90e985317b2525c0631a73d8e4c
JacobCruz-2004 <2004jacob.cruz@gmail.com>
2024-12-03 20:48:31 -0500

    removed test class

--- src/ca/sheridancollege/project/Deck.java
+++ src/ca/sheridancollege/project/Deck.java
@@ -15,33 +15,31 @@ public class Deck extends UnoGroupOfCards {
     public Deck(int size){super(size);}
     
     public void buildDeck(){
-//        GroupOfCards deck = new GroupOfCards(107);
+
        
         Color colors[]=Color.values();
         Value values[]=Value.values();
         for(Color color: colors){
             for(Value value:values){
                 if(value.equals(WILD)==false&&value.equals(WILD4)==false&&color.equals(NONE)==false&&value.equals(ZERO)==false){
-//                    Since there are two of each card, the same card is added twice to the deck.
+
                     getCards().add(new UnoCard(color, value));
                     getCards().add(new UnoCard(color, value));
-                   
+                }
+// There are 4 of each wild and wild4   
+                if(color.equals(NONE)&&(value.equals(WILD)||value.equals(WILD4))){
+                    getCards().add(new UnoCard(color, value));
+                    getCards().add(new UnoCard(color, value));
+                    getCards().add(new UnoCard(color, value));
+                    getCards().add(new UnoCard(color, value));
+                }
+// There are one zero for each color
+                if(color.equals(NONE)==false&&value.equals(ZERO)){
+                                        getCards().add(new UnoCard(color, value));
                 }
             }
         }
-//WILD and WILD4 are added manually because the only combinations of NONE and WILD/WILD4 are with themselves, not with the other colors and values
-        for(int i = 0;i<4;i++){getCards().add(new UnoCard(NONE, WILD));} //Adds four copies of WILD
-        for(int i = 0;i<4;i++){getCards().add(new UnoCard(NONE, WILD4));} //Adds four copies of WILD4
-        
-//Zeros are added separately because there is only one zero per color. 
-        for(Color color : colors){
-            if(color.equals(NONE)==false){
-            getCards().add(new UnoCard(color, ZERO));
-            }
-        }
-      
-        
-        
+
         
     }
     
