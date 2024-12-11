93ed34d0a9f31a1f8c844a2d6aa1c4273022ea72
hoangngoctram <tram1711@gmail.com>
2024-12-10 20:44:19 -0500

    Update PlayerView.java

--- src/ca/sheridancollege/project/PlayerView.java
+++ src/ca/sheridancollege/project/PlayerView.java
@@ -1,32 +1,130 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ca.sheridancollege.project;
 
+import static ca.sheridancollege.project.Color.*;
+import java.util.Scanner;
+
 /**
- *
- * @author HoangNguyen
- * @author Jacob Cruz
+ *@author HoangNguyen
+ * @author jacob
  */
 public class PlayerView {
- public void displayHand(Hand hand) {
-        System.out.println("Player's hand: " + hand.getSize() + " cards.");
+    public void displayHand(Hand hand) {
+        for(int i =0; i<hand.getCards().size();i++){
+            System.out.println("["+i+"]"+hand.getCards().get(i).toString());
+        }
+        System.out.print("[99]UNO");
+        System.out.print("\n[999]DRAW 1");
+        
+        
     }
 
     public void displayName(String name) {
         System.out.println("Player: " + name);
     }
 
-    public void selectCard() {
-        // Logic for selecting a card
+    public int selectCard(Hand hand) {
+        while(true){
+            try{
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Choose card to play or say UNO");
+                int select = sc.nextInt();
+                if(
+                    select<=hand.getCards().size()-1
+                    ||select==99
+                    ||select==999
+                   ){return select;}
+                
+                if(
+                    select>hand.getCards().size()-1
+                    ||select!=99
+                    ||select!=999){
+                    System.out.println("Invalid option, pick again");
+                    continue;
+                }
+                
+                    
+                
+                
+                
+
+            }
+
+            catch(Exception e){
+                System.out.println("Error: please enter int");
+                continue;
+            }
+        }   
     }
+    
 
     public void sayUno() {
         System.out.println("UNO!");
     }
 
-    public void displayScore(int score) {
-        System.out.println("Score: " + score);
+    public void displayScore(UnoPlayer player) {
+        System.out.println(player.getName()+"'s Score: "+player.getScore());
     }
+    
+    public String enterName(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name");
+        return sc.nextLine();
+    }
+    
+    public void pickAgain(){
+        System.out.println("Color or value does not match, choose another card or draw");
+    }
+    
+    public void playerGoesFirstMessage(){System.out.println("You go first!");}
+    public void playerGoesSecondMessage(){System.out.println("You go second");}
+    
+    public void aiAction(UnoCard card){
+        System.out.println("AI played: "+card.toString());
+    }
+    
+//    For wild and wild4
+    public Color pickColor(){
+        while(true){
+            try{
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Choose color: RED/BLUE/GREEN/YELLOW");
+                String input = sc.nextLine();
+                if(input.equals("RED")==false&&input.equals("BLUE")==false&&input.equals("GREEN")==false&&input.equals("YELLOW")==false){
+                    System.out.println("Error: input must be RED/BLUE/GREEN/YELLOW, choose again");
+                    continue;
+                }
+                
+                if(input.equals("RED")){
+                    return RED;
+                }
+                
+                if(input.equals("GREEN")){
+                    return GREEN;
+                }
+                
+                if(input.equals("BLUE")){
+                    return BLUE;
+                }
+                
+                if(input.equals("YELLOW")){
+                    return YELLOW;
+                }
+            }
+            catch(Exception e){
+                System.out.println("Error: input must be RED/BLUE/GREEN/YELLOW");
+                continue;
+            }
+        }
+    }
+    
+    public void aiCardsInHand(Hand aiHand){
+        System.out.println("Cards in hand: "+aiHand.getCards().size());
+    }
+    
+    public void aiDraw(){System.out.println("AI drew 1 card");}
 }
