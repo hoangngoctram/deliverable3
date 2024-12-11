9c0eb42465d8518cbbc08d595499534832b99fea
hoangngoctram <tram1711@gmail.com>
2024-12-10 20:42:54 -0500

    Update GameView.java

--- src/ca/sheridancollege/project/GameView.java
+++ src/ca/sheridancollege/project/GameView.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -5,16 +6,27 @@
 package ca.sheridancollege.project;
 
 /**
- *
- * @author cenanguyen
+ *@author Hoang Nguyen
+ * @author jacob
  */
 public class GameView {
-public void displayTopOfDiscardPile(Card card) {
-        System.out.println("Top of discard pile: " + card);
+    public void displayTopOfDiscardPile(Card card) {
+        System.out.println("\nTop of discard pile: " + card);
     }
 
     public void displayRoundNumber(int round) {
         System.out.println("Round: " + round);
     }
+    
+    public void displayTurnCount(int turnCount){
+        System.out.println("Turn "+turnCount);
+    }
+    
+    public void displayFinalWinner(UnoPlayer winner){
+        System.out.println("Winner: "+winner.getName());
+    }
+    
+    public void spacer(){System.out.println("_____________________________________________");}
+
 }
 
